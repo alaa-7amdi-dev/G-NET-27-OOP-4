d46a34c14ec4c5f5c007160ba5cb681939911247
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-03-01 14:47:56 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -8,17 +8,21 @@
             #region Part 01
 
             #region Q1
-             //Static Binding=> It meaning that the method call is resolved at compile time based on reference .
-             // it happen in :
-             // 1-Method overloding
-             // 2-Merthod Hiding
-             // 3-Static method
-             // 4-Private method 
+            //Static Binding=> It meaning that the method call is resolved at compile time based on reference .
+            // it happen in :
+            // 1-Method overloding
+            // 2-Merthod Hiding
+            // 3-Static method
+            // 4-Private method 
 
             // Dynamic Binding => It meaning that the method call is resolved at run time based on object.
             //1-Method overriding
             //2-Vircual method 
             //3-interface call
+            #endregion
+            #region Q2
+            //Method Overloading => Same method Different Parameters (order,type,number).
+            //Method Overriding => Same methof Different Impelementation.
             #endregion
             #endregion
         }
